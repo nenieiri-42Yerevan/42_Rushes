d62f7bfcb9e916a8e8d0dc85bcf540563a923e76
Volodya Ismailyan <nenie_iri@mail.ru>
2022-10-18 17:42:19 +0400

    rush02: norminette

--- Rush_02/ex00/read_all.c
+++ Rush_02/ex00/read_all.c
@@ -6,7 +6,7 @@
 /*   By: vismaily <nenie_iri@mail.ru>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 13:13:12 by vismaily          #+#    #+#             */
-/*   Updated: 2022/10/18 13:34:21 by vismaily         ###   ########.fr       */
+/*   Updated: 2022/10/18 17:39:46 by vismaily         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,12 +47,12 @@ char	*read_all(char *dict_name)
 	}
 	while (read(dict_fd, &cur_char, 1))
 		++nb_chars;
+	close(dict_fd);
 	str = (char *)malloc(sizeof(char) * (nb_chars + 1));
 	if (!str)
 	{
 		ft_putstr_fd("Error\n", 2);
 		return (0);
 	}
-	close(dict_fd);
 	return (fill_entire(dict_name, str));
 }
